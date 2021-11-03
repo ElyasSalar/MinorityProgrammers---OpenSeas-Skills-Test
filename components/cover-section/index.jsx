09769d258aaddc31a4cac4abaf2da7bdb4e66a5e
ElyasSalar <elyassalar8@gmail.com>
2021-11-03 18:50:30 +0300

    fix(key-iterator): passing the key in loops for react element

--- components/cover-section/index.jsx
+++ components/cover-section/index.jsx
@@ -12,7 +12,7 @@ export default function CoverSection({ imageName, heading, description, empty })
     <>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap" rel="stylesheet" />
       </Head>
       <Flex
